067492b740262785388091afab459e57d0967d4d
Abdulaziz AlShetwi <ecleeld@gmail.com>
2014-12-08 11:19:09 +0300

    Change file names.
    change date.rb to gregorian.rb to use date for hijri to be Hijri::Date

--- lib/hijri/date.rb
+++ lib/hijri/date.rb
@@ -1,22 +1,45 @@
-class Date
-  def to_hijri
-    Hijri::Hijri.new *Hijri::Converter.greo_to_hijri(self)
-  end
+module Hijri
+  class Hijri
+
+    attr_accessor :day, :month, :year
+    MONTHNAMES_EN = %w(Muharram Safar Rabia-Awwal Rabia-Thani Jumaada-Awal Jumaada-Thani Rajab Sha'ban Ramadan Shawwal Dhul-Qi'dah Dhul-Hijjah)
+    DAYNAMES = %w(as-Sabt al-Ahad al-Ithnayn ath-Thalaathaa al-Arba'aa' al-Khamis al-Jumu'ah)
   
-  def last_day_of_gregorian_month(month)
-    # Compute the last date of the month for the Gregorian calendar.
-    if month == 2
-      return 29 if (self.year % 4 == 0 && self.year % 100 != 0) || (self.year % 400 == 0)
+    def initialize(year=1, month=1, day=1)
+       @year, @month, @day = year, month, day
     end
-    return [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month - 1]
-  end
   
-  def to_abs
-    @d = self.day
-    @m = self.month - 1
-    @m.downto(1) do |m|
-      @d += last_day_of_gregorian_month(@m, year)
+    def islamic_leap_year?
+      return (((((11 * self.year) + 14) % 30) < 11) ? true : false)
     end
-    return (@d + 365 * (year - 1) + (year -1) / 4.0 - (year - 1) / 100.0 + (year - 1) / 400.0).to_i
+  
+    def last_day_of_islamic_month
+      # Last day in month during year on the Islamic calendar.
+      return ((self.month % 2 == 1) || (self.month == 12 && islamic_leap_year?) ? 30 : 29)
+    end
+  
+    def to_s
+      "#{@year}-#{sprintf('%02d', @month)}-#{sprintf('%02d', @day)}"
+    end
+  
+    def to_abs
+      month_days = 29 * (month - 1) # days on this year
+      nonleap_year_days  = 354 * (year - 1)
+      leap_year_days = (3 + (11 * year)) / 30.0
+      this_year  = (month / 2.0).to_i
+      
+      return (day + month_days + this_year + nonleap_year_days + leap_year_days + Hijri::ISLAMIC_EPOCH).to_i
+    end
+  
+    def to_greo
+      Date.new *Converter.hijri_to_greo(self)
+    end
+  
+    class << self
+      def today
+        d = Date.send(:now)
+        # Hijri.new.greo_to_hijri(d)
+      end
+    end
   end
 end
---
+++ lib/hijri/gregorian.rb
@@ -0,0 +1,22 @@
+class Date
+  def to_hijri
+    Hijri::Hijri.new *Hijri::Converter.greo_to_hijri(self)
+  end
+  
+  def last_day_of_gregorian_month(month)
+    # Compute the last date of the month for the Gregorian calendar.
+    if month == 2
+      return 29 if (self.year % 4 == 0 && self.year % 100 != 0) || (self.year % 400 == 0)
+    end
+    return [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month - 1]
+  end
+  
+  def to_abs
+    @d = self.day
+    @m = self.month - 1
+    @m.downto(1) do |m|
+      @d += last_day_of_gregorian_month(@m, year)
+    end
+    return (@d + 365 * (year - 1) + (year -1) / 4.0 - (year - 1) / 100.0 + (year - 1) / 400.0).to_i
+  end
+end
